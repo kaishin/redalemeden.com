8803fa9a840471304b884ce0b6c74aa6daa47c35
Reda Lemeden <git@kaishin.haz.email>
2018-03-15 00:06:34 +0100

    Add intro in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <section class="intro">
+    <div class="intro-content">
+      <h3 class="intro-tagline">
+        I spent the last decade designing software that doesn't confuse people.
+      </h3>
+
+      <p class="intro-paragraph">
+        Software that is <em>accessible</em>, <em>unambiguous</em>, <em>open</em>, and <em>respectful</em> to both its users and the platforms it runs on.
+      </p>
+    </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  </section>
 )
 
 export default IndexPage

972e6d0b7db0349a3db4a832de6a10eef2f586c9
Reda Lemeden <git@kaishin.haz.email>
2018-03-14 23:16:54 +0100

    Add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: 'Reda Lemeden',
+    author: 'Reda Lemeden'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    }
+  ]
 };

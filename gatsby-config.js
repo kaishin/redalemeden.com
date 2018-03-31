562607a8da7c86d07857ef1fc27ecc114c829558
Reda Lemeden <git@kaishin.haz.email>
2018-04-01 00:47:24 +0200

    Improve markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,16 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-abbr',
+          'gatsby-remark-autolink-headers',
+          'gatsby-remark-prismjs'
+        ]
+      }
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {

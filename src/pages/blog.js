670b3ad8153f0c8ee51e741a3c78e4fcd2519ecb
Reda Lemeden <git@kaishin.haz.email>
2018-04-12 23:59:54 +0200

    Add work and blog page stubs

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const BlogPage = () => (
+  <header class="main-header">
+    <h1>Blog</h1>
+    <div>Hello blog</div>
+  </header>
+)
+
+export default BlogPage
---
+++ src/pages/work.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const WorkPage = () => (
+  <header class="main-header">
+    <h1>Work</h1>
+    <div>Hello work</div>
+  </header>
+)
+
+export default WorkPage

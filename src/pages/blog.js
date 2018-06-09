5876cbce522324eecf41a7269059391cf7f646a6
Reda Lemeden <git@kaishin.haz.email>
2018-06-09 17:01:05 +0200

    Add page class to Blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Seo from '../components/seo'
+import Helmet from 'react-helmet'
 
 const BlogPage = () => (
   <header class="main-header">
-    <Seo title='Blog' />
+    <Helmet bodyAttributes={{class: "blog-page"}}/>
+    <Seo title='Blog' bodyClass='blog' />
     <h1>Blog</h1>
     <div>Hello blog</div>
   </header>

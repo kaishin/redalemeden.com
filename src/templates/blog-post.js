b3cd579d6a73f5a8047367c6fffa77ee0eb7cb5c
Reda Lemeden <git@kaishin.haz.email>
2019-01-18 00:18:00 +0100

    Start styling blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,63 +1,68 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Seo from '../components/seo'
-import DefaultLayout from '../components/layouts/default'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import Helmet from 'react-helmet';
+import Seo from '../components/seo';
+import DefaultLayout from '../components/layouts/default';
+import { Link, graphql } from 'gatsby';
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark
+    const post = this.props.data.markdownRemark;
 
     function swiftVersion(tags) {
-      var tag = tags.find(function (tag) {
-        return tag.includes('Swift-')
-      })
+      var tag = tags.find(function(tag) {
+        return tag.includes('Swift-');
+      });
 
-      if (typeof tag === "undefined") {
-        return false
+      if (typeof tag === 'undefined') {
+        return false;
       } else {
-        return tag.replace('-', ' ')
+        return tag.replace('-', ' ');
       }
     }
 
     return (
       <DefaultLayout>
         <Seo title={post.frontmatter.title} />
-        <Helmet bodyAttributes={{class: "post-page blog-page"}} />
+        <Helmet bodyAttributes={{ class: 'post-page blog-page' }} />
 
         <article className="content blog-content">
           <header className="post-header">
-            <div className="post-category">{post.frontmatter.category}</div>
+            <span className="post-category">{post.frontmatter.category}</span>
             <h1 className="post-title">
               <Link className="post-link" to={post.fields.slug}>
                 {post.frontmatter.title}
               </Link>
             </h1>
+            <div className="post-metadata">
+              <time date={post.frontmatter.date}>{post.frontmatter.formattedDate}</time> &mdash;{' '}
+              <span className="reading-time">{post.timeToRead}min read</span> &mdash;{' '}
+              {post.frontmatter.tags.join(' / ')}
+            </div>
           </header>
 
-          <aside className="post-metadata">
-            <time date={post.frontmatter.date}>
-              {post.frontmatter.formattedDate}
-            </time>
-            <span className="separator"> &mdash; </span>
-            <span className="reading-time">{post.timeToRead}min read</span>
-          </aside>
-
           <section className="full-post-content">
-            {swiftVersion(post.frontmatter.tags) && <span className="swift-version">The code snippets in this article are written using the <strong>{swiftVersion(post.frontmatter.tags)}</strong> syntax.</span>} 
+            {swiftVersion(post.frontmatter.tags) && (
+              <span className="swift-version">
+                The code snippets in this article are written using the{' '}
+                <strong>{swiftVersion(post.frontmatter.tags)}</strong> syntax.
+              </span>
+            )}
             <div dangerouslySetInnerHTML={{ __html: post.html }} />
 
             <div className="feedback-box">
-              Have feedback? Send it to <a target="_blank" href="https://twitter.com/intent/tweet?screen_name=kaishin" rel="noreferrer noopener">@kaishin</a>.
+              Have feedback? Send it to{' '}
+              <a target="_blank" href="https://twitter.com/intent/tweet?screen_name=kaishin" rel="noreferrer noopener">
+                @kaishin
+              </a>.
             </div>
           </section>
         </article>
       </DefaultLayout>
-    )
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -77,4 +82,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;

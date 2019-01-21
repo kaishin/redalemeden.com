e5bfe707dbae5297bd984548e7ed8d553d2c59d6
Reda Lemeden <git@kaishin.haz.email>
2019-01-22 00:51:57 +0100

    Remove Sass

--- src/components/layouts/standalone.js
+++ src/components/layouts/standalone.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Seo from '../seo'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Seo from '../seo';
 
-import '../../css/main.scss'
-import 'typeface-rubik'
+import '../../css/styles.css';
+import 'typeface-rubik';
 
 class StandaloneLayout extends React.Component {
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <React.Fragment>
         <Seo />
         {children}
       </React.Fragment>
-    )
+    );
   }
 }
 
 StandaloneLayout.propTypes = {
   children: PropTypes.node.isRequired
-}
+};
 
-export default StandaloneLayout
+export default StandaloneLayout;

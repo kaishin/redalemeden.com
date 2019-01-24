926fcf6c1f08e00daa1f887cfbf1cb45c02769a8
Reda Lemeden <git@kaishin.haz.email>
2019-01-25 00:54:15 +0100

    Fix contact page layout

--- postcss.config.js
+++ postcss.config.js
@@ -4,18 +4,20 @@ const postcssNormalize = require('postcss-normalize');
 const postcssExtend = require('postcss-extend');
 const postcssColorMod = require('postcss-color-mod-function');
 
-const from = './src/css/variables/glob.css';
+const from = './src/css/helpers/_glob.css';
 const to = './src/utils/glob-css.js';
 
 module.exports = () => ({
   plugins: [
     postcssNormalize(),
     postcssImport(),
-    postcssExtend(),
     postcssPresetEnv({
       stage: 0,
       importFrom: [ from ],
-      exportTo: [ to ]
+      exportTo: [ to ],
+      insertAfter: {
+        'nesting-rules': postcssExtend
+      }
     }),
     postcssColorMod()
   ]

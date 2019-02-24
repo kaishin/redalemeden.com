b55e4e4435548f7febece1c61d927747a742b358
Reda Lemeden <git@kaishin.haz.email>
2019-02-24 20:37:13 +0100

    Add language attribute

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Config from '../../gatsby-config.js'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Helmet from 'react-helmet';
+import Config from '../../gatsby-config.js';
+import PropTypes from 'prop-types';
 
 class Seo extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = props
+    super(props);
+    this.state = props;
   }
 
   render() {
-    const metadata = Config.siteMetadata
-    const description = this.state.description === undefined ? metadata.description : this.state.description
-    const title = this.state.title === undefined ? metadata.title : metadata.title + ' | ' + this.state.title
-    const keywords = this.state.keywords === undefined ? [] : this.state.keywords
+    const metadata = Config.siteMetadata;
+    const description = this.state.description === undefined ? metadata.description : this.state.description;
+    const title = this.state.title === undefined ? metadata.title : metadata.title + ' | ' + this.state.title;
+    const keywords = this.state.keywords === undefined ? [] : this.state.keywords;
 
     return (
       <Helmet
+        htmlAttributes={{
+          lang: 'en'
+        }}
         title={title}
         meta={[
           { name: 'robots', content: 'noodp, noydir' },
           { name: 'description', content: description },
-          { name: 'keywords', content:  keywords.concat(metadata.keywords).join(', ') },
+          { name: 'keywords', content: keywords.concat(metadata.keywords).join(', ') }
         ]}
       />
-    )
+    );
   }
 }
 
@@ -32,6 +35,6 @@ Seo.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   keywords: PropTypes.arrayOf.string
-}
+};
 
-export default Seo
+export default Seo;

702b30c52309d6770c4024738d8f678e64ab9242
Reda Lemeden <git@kaishin.haz.email>
2019-07-26 20:00:00 +0200

    Fix typo (Thanks Eli!)

--- src/components/microblog/header.js
+++ src/components/microblog/header.js
@@ -7,7 +7,7 @@ class MicroblogHeader extends React.Component {
       <aside className="blog-header">
         <h2 className="page-heading">Wide Gamut</h2>
         <p className="blog-intro">
-          A collection of micro posts, hot takes, and reflections about whatever happens to preoccupy me. Stay updaed
+          A collection of micro posts, hot takes, and reflections about whatever happens to preoccupy me. Stay updated
           using <Link to="/microblog.xml">RSS</Link>, <Link to="/microblog.json">JSON Feed</Link>,{' '}
           <a href="https://twitter.com/kaishin">Twitter</a>, or <a href="https://widegamut.club/kaishin">Mastodon</a>.
         </p>

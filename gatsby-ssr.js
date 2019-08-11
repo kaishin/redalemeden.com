6f82525a880f184f317ae29beb2775550310f0a1
Reda Lemeden <git@kaishin.haz.email>
2019-08-11 14:28:07 +0200

    Fix unique key warning

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,9 +2,9 @@ const React = require('react');
 
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <link rel="alternate" type="application/rss+xml" href="feed.xml" />,
-    <link rel="alternate" type="application/json" href="feed.json" />,
-    <link rel="alternate" type="application/rss+xml" href="microblog.xml" />,
-    <link rel="alternate" type="application/json" href="microblog.json" />
+    <link rel="alternate" type="application/rss+xml" href="feed.xml" key="1" />,
+    <link rel="alternate" type="application/json" href="feed.json" key="2" />,
+    <link rel="alternate" type="application/rss+xml" href="microblog.xml" key="3" />,
+    <link rel="alternate" type="application/json" href="microblog.json" key="4" />
   ]);
 };

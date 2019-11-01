ae27e217dd2df9438ed81edbc8230103c55e7793
Reda Lemeden <git@redalemeden.com>
2019-11-01 01:49:31 +0100

    Add dark mode

--- src/components/microblog/header.js
+++ src/components/microblog/header.js
@@ -8,7 +8,7 @@ class MicroblogHeader extends React.Component {
         <h2 className="page-heading">Wide Gamut</h2>
         <p className="blog-intro">
           A collection of micro posts, hot takes, and reflections about whatever happens to preoccupy me. Stay updated
-          using <Link to="/microblog.xml">RSS</Link>, <Link to="/microblog.json">JSON Feed</Link>,{' '}
+          using <Link to="/microblog.xml">RSS</Link>/<Link to="/microblog.json">JSON</Link> feeds,{' '}
           <a href="https://twitter.com/kaishin">Twitter</a>, or <a href="https://widegamut.club/kaishin">Mastodon</a>.
         </p>
       </aside>

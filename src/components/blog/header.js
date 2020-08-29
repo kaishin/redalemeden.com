8cc9c89ea6bb718fd8c3c295a4e4d159237b87a2
Reda Lemeden <git@redalemeden.com>
2020-08-29 21:02:07 +0200

    Update blog description

--- src/components/blog/header.js
+++ src/components/blog/header.js
@@ -5,7 +5,7 @@ class BlogHeader extends React.Component {
   render() {
     return (
       <PageHeader title="Unredacted">
-        I write about design, technology, programming, and other personal interests. Subscribe to the{' '}
+        A blog about design, technology, programming, and other personal interests. Subscribe to the{' '}
         <a href="/feed.xml">RSS</a> or <a href="/feed.json">JSON</a> feeds to get the latest articles.
       </PageHeader>
     );

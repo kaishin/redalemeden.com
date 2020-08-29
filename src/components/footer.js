0a4e4f655175003ebda07ecab4483d41c33626b9
Reda Lemeden <git@redalemeden.com>
2020-08-29 21:05:22 +0200

    Use static query on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,69 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 class Footer extends React.Component {
   render() {
-    const { author, siteUrl, email, authorBio } = this.props;
+    const { children } = this.props;
 
     return (
-      <footer className="site-footer">
-        <ul className="link-list">
-          <li className="link">
-            <a href="https://mastodon.social/@redalemeden" rel="me">
-              Mastodon
-            </a>
-            <span className="separator">/</span>
-          </li>
-          <li className="link">
-            <a href="https://github.com/kaishin" rel="me">
-              Github
-            </a>
-            <span className="separator">/</span>
-          </li>
-          <li className="link">
-            <a href="https://twitter.com/kaishin" rel="me">
-              Twitter
-            </a>
-          </li>
-        </ul>
-        <div className="h-card" style={{ display: 'none' }}>
-          <a className="p-name u-url" href={siteUrl}>
-            {author}
-          </a>
-          <img className="u-photo" src={siteUrl + '/author-photo.jpg'} alt={author} />
-          <p className="p-note">{authorBio}</p>
-          <a className="u-email" href={'mailto:' + email}>
-            {email}
-          </a>
-        </div>
-        <span className="copyright">
-          <em>&copy;</em> 2009-{new Date().getFullYear()} {author}.<br />
-          <small>
-            <a href="https://github.com/kaishin/redalemeden.com">Source Code</a>
-          </small>
-        </span>
-      </footer>
+      <StaticQuery
+        query={graphql`
+          query {
+            site {
+              siteMetadata {
+                author
+                siteUrl
+                email
+                authorBio
+              }
+            }
+          }
+        `}
+        render={(data) => (
+          <footer className="site-footer">
+            <ul className="link-list">
+              {children}
+              <li className="link">
+                <a href="https://github.com/kaishin" rel="me">
+                  Github
+                </a>
+                <span className="separator">/</span>
+              </li>
+              <li className="link">
+                <a href="https://mastodon.social/@redalemeden" rel="me">
+                  Mastodon
+                </a>
+                <span className="separator">/</span>
+              </li>
+              <li className="link">
+                <a href="https://twitter.com/kaishin" rel="me">
+                  Twitter
+                </a>
+              </li>
+            </ul>
+            <div className="h-card" style={{ display: 'none' }}>
+              <a className="p-name u-url" href={data.site.siteMetadata.siteUrl}>
+                {data.site.siteMetadata.author}
+              </a>
+              <img
+                className="u-photo"
+                src={data.site.siteMetadata.siteUrl + '/author-photo.jpg'}
+                alt={data.site.siteMetadata.author}
+              />
+              <p className="p-note">{data.site.siteMetadata.authorBio}</p>
+              <a className="u-email" href={'mailto:' + data.site.siteMetadata.email}>
+                {data.site.siteMetadata.email}
+              </a>
+            </div>
+            <span className="copyright">
+              <em>&copy;</em> 2009-{new Date().getFullYear()} {data.site.siteMetadata.author}.<br />
+              <small>
+                <a href="https://github.com/kaishin/redalemeden.com">Source Code</a>
+              </small>
+            </span>
+          </footer>
+        )}
+      />
     );
   }
 }

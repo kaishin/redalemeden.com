71030d4c68719b3aa9c378fb137783a53c57752d
Reda Lemeden <git@redalemeden.com>
2020-10-31 19:27:40 +0100

    Swap analytics script
    Use Plausible instead of Fathom.

--- src/html.js
+++ src/html.js
@@ -19,13 +19,7 @@ export default function HTML(props) {
         <meta name="msapplication-TileColor" content="#ffc40d" />
         <meta name="theme-color" content="#000000" />
         {props.headComponents}
-        <script
-          src="https://stats.redalemeden.com/tracker.js"
-          site="VKAGX"
-          included-domains="redalemeden.com"
-          honor-dnt="true"
-          defer
-        />
+        <script async defer data-domain="redalemeden.com" src="https://analytics.redalemeden.com/js/plausible.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}

893c554e426eb1d27d3426f2522b4443e7551e4a
Reda Lemeden <git@redalemeden.com>
2021-03-02 23:32:41 +0100

    Remove CMS code

--- src/cms/index.js
+++
@@ -1,5 +0,0 @@
-import CMS from 'netlify-cms-app';
-import WidegamutPathWidget from './widegamut-path-widget';
-
-CMS.registerWidget('widegamut-path-widget', WidegamutPathWidget);
-CMS.registerPreviewStyle('./style.css');

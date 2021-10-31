dd4434d5c4f5e0d1cec3cd988e18c78b2475975d
Reda Lemeden <git@redalemeden.com>
2021-10-31 16:16:09 +0100

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -4,17 +4,12 @@ const postcssNormalize = require('postcss-normalize');
 const postcssExtend = require('postcss-extend');
 const postcssColorMod = require('postcss-color-mod-function');
 
-const from = './src/css/helpers/_glob.css';
-const to = './src/utils/glob-css.js';
-
 module.exports = () => ({
   plugins: [
     postcssImport(),
     postcssNormalize(),
     postcssPresetEnv({
       stage: 0,
-      importFrom: [ from ],
-      exportTo: [ to ],
       insertAfter: {
         'nesting-rules': postcssExtend
       }

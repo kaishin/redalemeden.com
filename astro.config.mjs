50b445da0ede5ed4f2ed173aace04e36593cd47e
Reda Lemeden <git@redalemeden.com>
2025-03-15 19:15:35 +0100

    Refactor Tailwind styles in components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config

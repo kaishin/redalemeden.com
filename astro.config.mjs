556c3cb789018c263a5cc0a3854d964d1a89b126
Reda Lemeden <git@redalemeden.com>
2026-04-14 23:35:09 +0200

    Fix code block theming across light and dark modes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,10 @@ export default defineConfig({
 
   markdown: {
     shikiConfig: {
-      theme: "dracula",
+      themes: {
+        light: "github-light",
+        dark: "dracula",
+      },
       wrap: true,
     },
   },
